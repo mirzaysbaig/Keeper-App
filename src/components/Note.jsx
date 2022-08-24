32fac515b0b34c5929707f2777000b2d9d0891a0
Sador <Zerezghi@live.co.uk>
2022-08-24 01:26:54 +0100

    first commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1> This the main content</h1>
+            <p>
+                This is the note content
+            </p>
+        </div>
+    )
+}
+
+export default Note;
