582afcb571e56918afca1a6c00dcd1522ce96706
Sador1993 <Zerezghi@live.co.uk>
2022-10-28 01:26:48 +0000

    -

---
+++ build/precache-manifest.d1c8a6b733c3d42d07137fa98d13972f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ee1b8d4a8288ccec253fe1fbf6fc3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "625ae8448b8ad6bf83db",
+    "url": "/static/js/2.75fa1fe6.chunk.js"
+  },
+  {
+    "revision": "e6cf09fd0ace8f4f8997",
+    "url": "/static/js/main.9c853430.chunk.js"
+  },
+  {
+    "revision": "325f4b8f32369198a2c5",
+    "url": "/static/js/runtime-main.4e550677.js"
+  }
+]);
